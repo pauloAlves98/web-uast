a0577e6a2a6242ac52fab6b08080790561092ce7
ElvisNogueira <41121143+ElvisNogueira@users.noreply.github.com>
2020-05-05 02:35:44 -0300

    Projeto cronometro

---
+++ ElvisNogueira/Cronometro/script.js
@@ -0,0 +1,48 @@
+var segundos = 0;
+var minutos = 0;
+var hora = 0;
+var tempo = document.getElementById('tempo');
+var intervalo;
+
+function iniciarParar(){
+    
+    var acao = document.getElementById('play');
+    if(acao.innerText == "Iniciar"){
+        acao.innerText = "Pausar";
+        acao.style.border = "solid 1px yellow"
+        acao.style.color = "yellow"
+        intervalo = setInterval('contar()',1000)
+    }else{
+        acao.innerText = "Iniciar";
+        acao.style.border = "solid 1px green"
+        acao.style.color = "green"
+        clearInterval(intervalo);
+    }
+}
+
+function parar(){
+    clearInterval(intervalo);
+    tempo.innerText = "00:00:00"
+}
+
+function contar(){
+    
+    segundos++;
+    if(segundos>59){
+        segundos = 0;
+        minutos++;
+    }
+    if(minutos>59){
+        minutos = 0;
+        hora++;
+    }    
+
+    tempo.innerText = addZero(hora)+":"+addZero(minutos)+":"+addZero(segundos)
+}
+
+function addZero(valor){
+    if(valor<10){
+        return "0"+valor
+    }
+    return valor;
+}
